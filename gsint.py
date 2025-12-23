d5551bfe98e12dac522b06fa5b4caa538a698d68
GoxShiva <151367976+GoxShiva@users.noreply.github.com>
2025-12-23 19:22:08 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- gsint.py
+++ gsint.py
@@ -72,7 +72,7 @@ def logo():
     `YMMNq.   `YMMNq.   MM" "bg. 
   .     `MM .     `MM   MM    `Y 
   Mb     dM Mb     dM   MM    ,9 
-  P"Ybmmd"  P"Ybmmd"  .JMMmmmd9.    INSTAGRAM AC8 CLON3
+  P"Ybmmd"  P"Ybmmd"  .JMMmmmd9.    {G}INSTAGRAM AC8 CLON3
 -----------------------------------------
  FACEBOOK  :     \033[1;97mShivaa Lodiwal 
  WHATAPP   :     \033[1;97m+918000032877
@@ -201,4 +201,5 @@ def graphrm(id,psd,tid):
 	lop+=1
 Main()
 #-৳-৳-৳-৳
+
 
